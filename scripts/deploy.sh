8280e13832c0d9e7853260b1fe74fd5a34fe4027
Gilles Lasnier <gilles.lasnier@gmail.com>
2017-12-04 17:04:32 +0100

    root: fix folders and files deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,8 +4,20 @@ set -e
 
 DIR=$(pwd)
 
-mkdir -p ${DIR} api/{views,models,controllers} bin/www documenation encryption public/{images,javascripts,stylesheets}
+mkdir -p ${DIR} api/{views,models,controllers} bin/www documentation encryption public/{images,javascripts,stylesheets}
 
 touch .gitignore app_server.js INSTALL LICENSE Makefile  package.json README.md
+
+EMPTY_DIRS=$(find . -type d -empty -not -path "./.git/*")
+for item in "${EMPTY_DIRS[@]}"
+do
+    echo ${item}
+    curd=$(pwd)
+    cd ${item}
+    touch .empty
+    cd ${curd}
+done
+
+tree .
 
 exit $?
